26e08eb5bdd7ad0d81fa66ebe5e8429f275fd6d4
Jiří Pihrt <jira.pihrt@volny.cz>
2019-08-01 22:31:30 +0200

    fix annoy-o-tron for kryndy

--- tribalwars_scripts/annoy-o-tron/main.js
+++ tribalwars_scripts/annoy-o-tron/main.js
@@ -5,52 +5,91 @@ function showErrorMessage(message) {
     $("#error-message").show();
 }
 
-$("#content_value").load("https://darxeal.github.io/tribalwars_scripts/annoy-o-tron/form.html", function() {
+$("#content_value").html(`
+<table class="vis">
+<tr>
+    <th colspan="2">Annoy-o-Tron</th>
+</tr>
+<td>
+    <img src="https://gamepedia.cursecdn.com/hearthstone_gamepedia/thumb/b/b2/Annoyotron_full.jpg/400px-Annoyotron_full.jpg?version=7724bf667f441932bc609f49473f0377">
+</td>
+<td id="annoy-content">
+    <div><i>
+        Skript pošle všechny jednotky z vesnice kterou máš teď aktivní, a po chvilce útok zruší.
+        Takto pokračuje v náhodných intervalech dokud nezavřeš tuhle kartu.
+        <b>Z této vesnice nesmí jít žádný jiný příkaz!</b>
+    </i></div>
+    <br>
+    <table>
+        <tr>
+            <td>Cíl:</td>
+            <td><input type="text" name="target" id="input-target" value="XXX|YYY"></td>
+        </tr>
+        <tr>
+            <td>První útok se odešle za (minuty)</td>
+            <td><input type="number" name="delay" id="input-delay" value="120"></td>
+        </tr>
+        <tr>
+            <td>Minimální interval (minuty)</td>
+            <td><input type="number" name="interval-min" id="input-interval-min" value="30"></td>
+        </tr>
+        <tr>
+            <td>Maximální interval (minuty)</td>
+            <td><input type="number" name="interval-max" id="input-interval-max" value="60"></td>
+        </tr>
+        <tr>
+            <td>Útok zrušit po (sekundy)</td>
+            <td><input type="number" name="cancel-timeout" id="input-cancel-timeout" value="3"></td>
+        </tr>
+    </table>
+    <div id="error-message" style="color: red; display: none;"></div>
+    <div><button id="btn-confirm" class="btn">Potvrdit</button></div>
+</td>
+</table>`);
     
-    $("#btn-confirm").click(function() {
+$("#btn-confirm").click(function() {
+
+    console.log("click");
+    $("error-message").hide();
+
+    var coords = $("#input-target").val().split("|");
+
+    if (coords.length != 2)
+        return showErrorMessage("Cíl je v nesprávném formátu.");
+
+    var attack = {
+        source_id: game_data.village.id,
+        tx: coords[0],
+        ty: coords[1],
+        units: {}
+    };
+
+    var delay = Number($("#input-delay").val()) * 60 * 1000;
+    var intervalMin = Number($("#input-interval-min").val()) * 60 * 1000;
+    var intervalMax = Number($("#input-interval-max").val()) * 60 * 1000;
+    var cancelTimeout = Number($("#input-cancel-timeout").val()) * 1000;
+
+    if (delay <= 0 || intervalMin <= 0 || intervalMax <= 0 || cancelTimeout <= 0)
+        return showErrorMessage("Hodnoty musí být větší než 0.");
+    if (intervalMin > intervalMax)
+        return showErrorMessage("Minimální interval nesmí být větší než maximální interval.");
+
+
+    var interval;
     
-        console.log("click");
-        $("error-message").hide();
-    
-        var coords = $("#input-target").val().split("|");
-    
-        if (coords.length != 2)
-            return showErrorMessage("Cíl je v nesprávném formátu.");
-    
-        var attack = {
-            source_id: game_data.village.id,
-            tx: coords[0],
-            ty: coords[1],
-            units: {}
-        };
-    
-        var delay = Number($("#input-delay").val()) * 60 * 1000;
-        var intervalMin = Number($("#input-interval-min").val()) * 60 * 1000;
-        var intervalMax = Number($("#input-interval-max").val()) * 60 * 1000;
-        var cancelTimeout = Number($("#input-cancel-timeout").val()) * 1000;
-    
-        if (delay <= 0 || intervalMin <= 0 || intervalMax <= 0 || cancelTimeout <= 0)
-            return showErrorMessage("Hodnoty musí být větší než 0.");
-        if (intervalMin > intervalMax)
-            return showErrorMessage("Minimální interval nesmí být větší než maximální interval.");
-    
-    
-        var interval;
-        
-        var attackAndCancelFunction = function() {
-            interval = Math.floor(Math.random() * (intervalMax - intervalMin + 1)) + intervalMin;
-            attackVillage(attack);
-            $("#annoy-content").append("<div>Útok poslán.</div>");
-            setTimeout(cancelAttack, cancelTimeout);
-        };  
-    
-        setTimeout(function() {
-            attackAndCancelFunction();
-            setInterval(attackAndCancelFunction, interval);
-        }, delay);
-    
-        $("#btn-confirm").attr("disabled", true);
-    });
+    var attackAndCancelFunction = function() {
+        interval = Math.floor(Math.random() * (intervalMax - intervalMin + 1)) + intervalMin;
+        attackVillage(attack);
+        $("#annoy-content").append("<div>Útok poslán.</div>");
+        setTimeout(cancelAttack, cancelTimeout);
+    };  
+
+    setTimeout(function() {
+        attackAndCancelFunction();
+        setInterval(attackAndCancelFunction, interval);
+    }, delay);
+
+    $("#btn-confirm").attr("disabled", true);
 });
 
 
